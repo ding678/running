9c2dda11eb33947a4ce1413a8d1885582c9ef6b1
ding678 <1174945475@qq.com>
2018-03-19 19:06:06 +0800

    --all

---
+++ pages/common/signup/routeMap/routeMap.js
@@ -0,0 +1,68 @@
+// pages/common/signup/routeMap/routeMap.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    polyline:[{
+      point: [{ latitude: 116.402359, longitude: 39.999763 }, { latitude: 116.402122, longitude: 39.929583 }, { latitude: 116.396832, longitude: 39.999271 }]
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
